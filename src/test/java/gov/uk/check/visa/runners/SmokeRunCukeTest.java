d23bfd6ac9d2166374b138cc30e6c638d454b500
niprimesh <147188559+niprimesh@users.noreply.github.com>
2023-12-19 20:36:26 +0000

    Nimesh SW6

---
+++ src/test/java/gov/uk/check/visa/runners/SmokeRunCukeTest.java
@@ -0,0 +1,17 @@
+package gov.uk.check.visa.runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+public class SmokeRunCukeTest {
+    @CucumberOptions(
+            features = "src/test/resources/features",
+            glue = "gov/uk/check/visa/steps",
+            plugin = {"html:target/cucumber-reports/cucumber.html",
+                    "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+            tags = "@smoke"
+    )
+
+    public class RunCukeTest extends AbstractTestNGCucumberTests {
+    }
+}
